4ae4add83a25adce87e6e983d008915fc0a04ea7
wiktomi <74141843+wiktomi@users.noreply.github.com>
2022-01-06 23:05:59 +0100

    Refactoring

--- YachtRent/Controllers/CustomersController.cs
+++ YachtRent/Controllers/CustomersController.cs
@@ -46,7 +46,7 @@ namespace YachtRent.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CustomerID,CustomerName,Country,MobilePhone")] Customers customers)
+        public ActionResult Create([Bind(Include = "CustomerName,Country,MobilePhone")] Customers customers)
         {
             if (ModelState.IsValid)
             {
